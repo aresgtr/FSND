f7624e2fd9d6f8112178f3fd37688c662fa3abc4
Qi <784359442@qq.com>
2020-06-12 16:28:10 -0700

    Finish frontend

--- projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/starter_code/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
+    url: 'zhangqi', // the auth0 domain prefix
+    audience: 'drink', // the audience set for the auth0 app
+    clientId: 'H95amjK7SrFfrfwptdNM3ROBiqaRYrpF', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
   }
 };
